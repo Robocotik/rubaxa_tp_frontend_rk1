52256f598b9ae533a0c5d8d410479c316af8537b
Robocotik <alexander.starkin@gmail.com>
2025-10-12 17:59:37 +0300

    [feat] parallel:  remove webWorkers logic

--- parallel.js
+++ parallel.js
@@ -1,15 +1,5 @@
 // Параллельные вычисления
 
-function getWorkerCode(task) {
-  return `
-      onmessage = function(e) {
-        const task = ${task};
-        task((result) => {
-            postMessage({ index: e.data.index, result: result }); 
-      })};
-    `;
-}
-
 function parallel(tasks, onAllResolved) {
   const results = new Array(tasks.length);
   let completed = 0;
@@ -25,38 +15,12 @@ function parallel(tasks, onAllResolved) {
       completed++;
       if (completed === tasks.length) onAllResolved(results);
     } else {
-      let syncCompleted = false; 
-      // этот ужас существует, потому что в тестах
-      //на sync существуют обычные sync callback и мы никак не можем узнать, 
-      // что это не async callback пока не проверим
-
-      task(result => {
-        syncCompleted = true;
-        results[index] = result;
-        completed++;
-        if (completed === tasks.length) onAllResolved(results);
-      });
-
-      setTimeout(() => {
-        if (!syncCompleted) {
-          const workerCode = getWorkerCode(task);
-          const blob = new Blob([workerCode], {type: 'application/javascript'});
-          const blobUrl = URL.createObjectURL(blob);
-          const worker = new Worker(blobUrl);
-
-          worker.onmessage = function (e) {
-            if (!syncCompleted) {
-              syncCompleted = true;
-              results[e.data.index] = e.data.result;
-              completed++;
-              worker.terminate();
-              URL.revokeObjectURL(blobUrl);
-              if (completed === tasks.length) onAllResolved(results);
-            }
-          };
-
-          worker.postMessage({index: index});
-        }
+      setTimeout(() => { // у нас может быть как sync так и async callback, так что обоих оборачиваем в setTimeout(код,0)
+        task(result => {
+          results[index] = result;
+          completed++;
+          if (completed === tasks.length) onAllResolved(results);
+        });
       }, 0);
     }
   });
